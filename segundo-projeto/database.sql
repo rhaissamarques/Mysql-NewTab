3be6d432db96f749ac2dc20a116b972acb3dcc35
Rhaissa Marques <rhaissa.marques@gmail.com>
2021-11-13 15:10:44 -0300

    segunda tabela, cartao

--- segundo-projeto/database.sql
+++ segundo-projeto/database.sql
@@ -10,3 +10,13 @@ CREATE TABLE usuario (
     data_cadastro date, 
     primary key (Id) 
 );
+
+CREATE TABLE cartao ( 
+    Id int NOT NULL auto_increment, 
+    Numero decimal(20) NOT NULL, 
+    Cvv decimal(3) NOT NULL, 
+    Data_de_expiracao date, 
+    Valido boolean NOT NULL,
+    Data_de_cadastro date,
+    primary key (Id) 
+);

0aee57ea175856186792d528a02f66d0613d7e23
Rhaissa Marques <rhaissa.marques@gmail.com>
2021-11-13 15:04:36 -0300

    correção imagem

--- segundo-projeto/database.sql
+++ segundo-projeto/database.sql
@@ -5,8 +5,8 @@ use newtab_projeto2;
 CREATE TABLE usuario ( 
     Id int NOT NULL auto_increment, 
     Nome varchar(30) NOT NULL, 
-    Imagem varchar(50) NOT NULL, 
-    Nome de usuario varchar(50) NOT NULL, 
+    Imagem NOT NULL, 
+    Nome_de_usuario varchar(50) NOT NULL, 
     data_cadastro date, 
     primary key (Id) 
 );

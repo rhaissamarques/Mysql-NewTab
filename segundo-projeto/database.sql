8d0ab2f633a27ce9dbb46452adfdb551cf8308dc
Rhaissa Marques <rhaissa.marques@gmail.com>
2021-11-13 15:00:14 -0300

    criando primeira tabela

---
+++ segundo-projeto/database.sql
@@ -0,0 +1,12 @@
+CREATE database newtab_projeto2;
+
+use newtab_projeto2;
+
+CREATE TABLE usuario ( 
+    Id int NOT NULL auto_increment, 
+    Nome varchar(30) NOT NULL, 
+    Imagem varchar(50) NOT NULL, 
+    Nome de usuario varchar(50) NOT NULL, 
+    data_cadastro date, 
+    primary key (Id) 
+);

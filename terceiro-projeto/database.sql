93b9a64a54e52cee12b5438c49df5cff775a3020
Rhaissa Marques <rhaissa.marques@gmail.com>
2021-11-13 18:56:57 -0300

    insert tabela usuario

--- terceiro-projeto/database.sql
+++ terceiro-projeto/database.sql
@@ -10,3 +10,14 @@ CREATE TABLE usuario (
     Hora_hashtag time, 
     primary key (Id) 
 );
+
+INSERT INTO usuario (Squad, Hashtag, Data_hashtag, Hora_hashtag) values ("5", "porsche", "02/09/2021", "19:47");
+INSERT INTO usuario (Squad, Hashtag, Data_hashtag, Hora_hashtag) values ("5", "futebol", "02/09/2021", "19:27");
+INSERT INTO usuario (Squad, Hashtag, Data_hashtag, Hora_hashtag) values ("5", "neymar", "02/09/2021", "10:40");
+INSERT INTO usuario (Squad, Hashtag, Data_hashtag, Hora_hashtag) values ("5", "homem aranha", "24/09/2021", "20:52");
+
+SELECT * FROM usuario;
+
+UPDATE usuario SET Hashtag = "homem de ferro" WHERE Id = 4;
+
+DELETE FROM usuario WHERE Id = 1;

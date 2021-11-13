9bb3879116ba85c2d76a5c0d26b9d2e3c8170fb7
Rhaissa Marques <rhaissa.marques@gmail.com>
2021-11-13 18:59:28 -0300

    mudando nome da tabela

--- terceiro-projeto/database.sql
+++ terceiro-projeto/database.sql
@@ -2,7 +2,7 @@ CREATE database newtab_projeto3;
 
 use newtab_projeto3;
 
-CREATE TABLE usuario ( 
+CREATE TABLE post_on_twitter ( 
     Id int NOT NULL auto_increment, 
     Squad int NOT NULL, 
     Hashtag varchar(150), 
@@ -11,13 +11,13 @@ CREATE TABLE usuario (
     primary key (Id) 
 );
 
-INSERT INTO usuario (Squad, Hashtag, Data_hashtag, Hora_hashtag) values ("5", "porsche", "02/09/2021", "19:47");
-INSERT INTO usuario (Squad, Hashtag, Data_hashtag, Hora_hashtag) values ("5", "futebol", "02/09/2021", "19:27");
-INSERT INTO usuario (Squad, Hashtag, Data_hashtag, Hora_hashtag) values ("5", "neymar", "02/09/2021", "10:40");
-INSERT INTO usuario (Squad, Hashtag, Data_hashtag, Hora_hashtag) values ("5", "homem aranha", "24/09/2021", "20:52");
+INSERT INTO post_on_twitter (Squad, Hashtag, Data_hashtag, Hora_hashtag) values ("5", "porsche", "02/09/2021", "19:47");
+INSERT INTO post_on_twitter (Squad, Hashtag, Data_hashtag, Hora_hashtag) values ("5", "futebol", "02/09/2021", "19:27");
+INSERT INTO post_on_twitter (Squad, Hashtag, Data_hashtag, Hora_hashtag) values ("5", "neymar", "02/09/2021", "10:40");
+INSERT INTO post_on_twitter (Squad, Hashtag, Data_hashtag, Hora_hashtag) values ("5", "homem aranha", "24/09/2021", "20:52");
 
-SELECT * FROM usuario;
+SELECT * FROM post_on_twitter;
 
-UPDATE usuario SET Hashtag = "homem de ferro" WHERE Id = 4;
+UPDATE post_on_twitter SET Hashtag = "homem de ferro" WHERE Id = 4;
 
-DELETE FROM usuario WHERE Id = 1;
+DELETE FROM post_on_twitter WHERE Id = 1;
